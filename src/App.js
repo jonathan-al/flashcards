ebe764904b537cb3c45caf6f3ec0d92a82c1c96c
Jonathan Alcantara <jonathan.alcantara@improving.com>
2023-12-18 16:50:16 -0600

    Implement Flashcards component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useState } from "react"
+import { questions } from "./data"
+
+console.log(questions)
+
+const Flashcards = () => {
+  const [selectedId, setSelectedId] = useState(null)
+  console.log(questions[0])
+
+  const selectHandler = (id) => {
+    setSelectedId(id !== selectedId ? id : null)
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          className={question.id === selectedId ? "selected" : ""}
+          onClick={() => selectHandler(question.id)}
+        >
+          <p>
+            {question.id === selectedId
+              ? question.answer
+              : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
 const App = () => {
   return (
     <>
-      <h1>Hello React!</h1>
+      <Flashcards />
     </>
   )
 }
